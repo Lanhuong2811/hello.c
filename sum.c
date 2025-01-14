ee0cdc722208eede36b0eb163832353b234e6890
Lanhuong2811 <trinhlanhuong2895@gmail.com>
2025-01-14 22:05:29 +0700

    Create sum.c
    bài toán tổng

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+int main () {
+int num1, num2, sum;
+printf("enter first interger: ");
+scanf("%d, &num1);
+printf(enter second interger: ");
+scanf("%d", &num2);
+sum = num1 + num2
+printf ("sum of %d and %d is %d\n", num1, num2, sum);
+return 0;
+}
